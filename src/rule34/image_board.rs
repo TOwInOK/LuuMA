dd34ee64dd6bf98200b2de6bb2a42bf6d14e22ca
TOwInOK 60252419+TOwInOK@users.noreply.github.com <60252419+TOwInOK@users.noreply.github.com>
2025-01-07 22:26:18 +0300

    rewrite rule34 parsing

--- src/rule34/image_board.rs
+++ src/rule34/image_board.rs
@@ -1,14 +1,13 @@
 use crate::{rule34::paginate, Context, Error};
 use shuller::prelude::*;
-use tracing::debug;
 
-static LEN: usize = 4;
-static LEN_MAX: usize = 30;
+static MAX_TAB_LEN: usize = 4;
+static MAX_SEARCH_LEN: u16 = 30;
 
 /// R34
-/// Video => animated & sound, to positive tags
-/// Gif => gif, to positive tags
-/// Img => any stuff
+/// - Video => animated & sound, to positive tags
+/// - Gif => gif, to positive tags
+/// - Img => any stuff
 #[poise::command(
     slash_command,
     guild_only,
@@ -36,64 +35,68 @@ pub async fn porno(
     #[description_localized("ru", "Сколько выдать?")]
     #[name_localized("ru", "количество")]
     size: Option<usize>,
+    #[description = "Need to show tags?"]
+    #[description_localized("ru", "Нужно ли отображать теги?")]
+    #[name_localized("ru", "показать_теги")]
+    show_tags: bool,
 ) -> Result<(), Error> {
+    // Typing in chanel
     ctx.defer_ephemeral().await?;
-    let mut posts = vec![];
-    let size = size.unwrap_or(3);
-    if size > LEN {
+
+    // init start bath of pictures
+    let size = size.unwrap_or(4);
+
+    // check max buttons (max 4)
+    if size > MAX_TAB_LEN {
         return Err("**Error: TOOO BIG**, max size is 4".into());
     }
+
+    // return in find
     if let Some(id) = id {
-        debug!("found some id: {}", &id);
-        let mut data = match R34!(D; id = id) {
+        // search id
+        let posts = match R34!(D; id = id) {
             Ok(e) => e.data(),
             Err(_) => return Err("**Error: Posts** not found".into()),
         };
-        posts.append(&mut data);
-    } else if positive_tags.is_some() || negative_tags.is_some() {
-        debug!(
-            "found some tags: p = {:#?}, n = {:#?}",
-            &positive_tags, &negative_tags
-        );
-        let a = R34!(D;
-            p = positive_tags.unwrap_or("".to_string()).split_whitespace().collect(),
-            // p = ["ai_generated"].to_vec(),
-                n = negative_tags.unwrap_or("".to_string()).split_whitespace().collect(),
-                // n = [].to_vec(),
-                limit = LEN_MAX as u16
-        );
-        match a {
-            Ok(r) => {
-                let mut a = r.shuffle().data();
-                posts.append(&mut a);
-            }
-            Err(e) => return Err(format!("**Error: Posts** not found\nErr{}", e).into()),
+        // if empty
+        if posts.is_empty() {
+            return Err("**Error: Posts** not found".into());
         }
-    } else {
-        debug!("Nothing addition params found");
-        match shuller::rules::rule34::params::R34Params::init()
-            .page(random_usize!(u16::MAX as usize) as u16)
-            .limit(size as u16)
-            .download()
-            .await
-        {
-            Ok(data) => posts.append(&mut data.data()),
-            Err(_) => return Err("**Error: Posts** not found".into()),
-        }
+        // gen tab
+        return paginate::paginate(ctx, &posts).await;
     }
 
+    // positive tags
+    let mut pt: Vec<&str> = vec![];
+    // negative tags
+    let mut nt: Vec<&str> = vec![];
+
+    // build query
+
+    let positive_tags = positive_tags.unwrap_or_default().to_lowercase();
+    let negative_tags = negative_tags.unwrap_or_default().to_lowercase();
+
+    // push positive
+    for item in positive_tags.split_whitespace() {
+        pt.push(item)
+    }
+
+    // push negative
+    for item in negative_tags.split_whitespace() {
+        nt.push(item)
+    }
+
+    // fetch posts with tags
+    let posts = R34!(D; p = pt, n = nt, limit = MAX_SEARCH_LEN)
+        .map_err(|x| Error::from(format!(r#"**Error: Posts** not found. Err{}"#, x)))?;
     if posts.is_empty() {
         return Err("**Error: Posts** not found".into());
     }
-    paginate::paginate(
-        ctx,
-        if posts.len() < 4 {
-            &posts[..posts.len()]
-        } else {
-            &posts[..4]
-        },
-    )
-    .await?;
+    // shuffle
+    let mut posts_shuffled = posts.shuffle().data();
+    if posts_shuffled.len() > MAX_TAB_LEN {
+        posts_shuffled.truncate(MAX_TAB_LEN)
+    }
 
-    Ok(())
+    paginate::paginate(ctx, &posts_shuffled).await
 }
